a7f4919a9c8034f23b0f509029b7f54beb483b43
kushal5556 <demonxcoc95@gmail.com>
2026-08-14 16:25:36 +0545

    Added new textures

--- main.c
+++ main.c
@@ -10,12 +10,12 @@
 #define MAX(x,y) (x > y ? x : y)
 #define MIN(x,y) (x < y ? x : y)
 
-// ---- Global Variable -------------
+// ---- Global Constants-------------
 #define BLOCK_WIDTH 30
 #define BLOCK_HEIGHT 30
 
 #define  CHUNK_WIDTH 10
-#define  CHUNK_HEIGHT 10
+#define  CHUNK_HEIGHT 20
 
 #define CHUNK_DISTANCE 1 //chunk one both sides (not including current)
 
@@ -23,17 +23,24 @@
 #define RAY_STEP 10 
 #define RAY_RADIUS 5.0f
 
-#define GRAVITY 200.0f
+#define GRAVITY 300.0f
 
 #define WORLD_SIZE 10
+
 //----- camera ----
 Vector2 CAMERA;
 
 //----------- structs -----------
 typedef enum{
 	AIR = 0,
-	DIRT,
+	GRASS,
 	STONE,
+	WATER,
+	BEDROCK,
+	DIAMOND_ORE,
+	GOLD_ORE,
+	EMERALD_ORE,
+	LAVA,
 }BlockType;
 
 typedef struct{
@@ -48,9 +55,14 @@ typedef struct{
 }Chunk;
 
 typedef struct{
+	Chunk **items;
+	size_t size;
+	size_t capacity;
+}World;
+
+typedef struct{
 	Vector2 position;
 	Vector2 velocity;
-	Vector2 ray;
 }Player;
 
 typedef struct{
@@ -65,29 +77,41 @@ static float breakProgress = 0.0f;
 const float TIME_TO_BREAK = 0.8f; 
 Vector2 hitPosition = {0};
 
+static float worldSeedOffset = 0.0f;
+
+
+
 // ----- function declaration -----------------
+void InitializeWorldSeed();
+
+void init_chunk(Chunk* chunk);
 void init_world(Chunk chunks[], int size);
 void draw_chunk(Chunk* chunk);
 
-//---chunk edits ---
+//---chunk edit---
 int chunk_coord(float posX);
 int chunk_index(int chunk_coord);
 
 int getLeftChunkIndex(int chunkIdx);
 int getRightChunkIndex(int chunkIdx);
 
+// block edit
 void breakBlock();
-
+void placeBlock(Chunk chunks[], Vector2 playerPos, Vector2 playerSize, BlockType type);
 void findHoveredBlock(Chunk chunks[], Vector2 playerPos, Vector2 playerSize);
 
 DirectionXY_i blockPlayerDirection(Chunk chunks[], Vector2 playerPosition, int blockChunkIdx);
 void placeBlockAt(Chunk chunks[], int blockChunkIdx, int targetX, int targetY, BlockType type, Vector2 playerPos, Vector2 playerSize);
 bool isBlockBreak(Chunk chunks[], int blockX, int blockY, int chunkIdx);
 
+// world chunks edit
+BlockType noise(float wX, float wY);
+
 void add_chunk(Chunk chunks[], Vector2 pos);
 
 // collision function
 bool player_collided(Chunk chunks[], Vector2 position, Vector2 size);
+
 bool AABB(Vector2 posA, Vector2 sizeA, Vector2 posB, Vector2 sizeB);
 bool rect_circle_collision(Vector2 rectPos, Vector2 rectSize, Vector2 circlePos, float circleRadius);
 bool point_rect_collision(Vector2 point, Vector2 rectPos, Vector2 rectSize);
@@ -96,6 +120,13 @@ bool point_rect_collision(Vector2 point, Vector2 rectPos, Vector2 rectSize);
 Texture2D grassBlock;
 Texture2D stoneBlock;
 Texture2D steve;
+Texture2D bedRock;
+Texture2D waterBlock;
+Texture2D lavaBlock;
+Texture2D diamondOre;
+Texture2D goldOre;
+Texture2D ironOre;
+Texture2D emeraldOre;
 
 int main()
 {
@@ -105,6 +136,13 @@ int main()
 	grassBlock = LoadTexture("Textures/GrassBlock.png");
 	stoneBlock = LoadTexture("Textures/StoneBlock.png");
 	steve      = LoadTexture("Textures/steve.png");
+	bedRock    = LoadTexture("Textures/BedRock.png");
+	waterBlock = LoadTexture("Textures/WaterBlock.png");
+	lavaBlock  = LoadTexture("Textures/LavaBlock.png");
+	diamondOre = LoadTexture("Textures/DiamondOre.png");
+	ironOre    = LoadTexture("Textures/IronOre.png");
+	emeraldOre = LoadTexture("Textures/EmeraldOre.png");
+	goldOre    = LoadTexture("Textures/GoldOre.png");
 
 	Chunk chunks[WORLD_SIZE];
 	init_world(chunks, WORLD_SIZE);
@@ -120,14 +158,14 @@ int main()
 		// ------------------ update -------------------
 		float dt = GetFrameTime();
 		float speed = 80;
-		float damping = 0.99f;
+		float damping = 0.98f;
 		player.velocity.y += GRAVITY * dt;
 		player.velocity.x *= damping;
 		player.velocity.y *= damping;
 
 
 		if(IsKeyPressed(KEY_SPACE)){
-			player.velocity.y -= ((GRAVITY/2)+10)*speed * dt;
+			player.velocity.y -= ((GRAVITY/2)+40)*speed * dt;
 		}
 		if(IsKeyDown(KEY_S)){
 			player.velocity.y += speed * dt;
@@ -138,6 +176,7 @@ int main()
 		if(IsKeyDown(KEY_A)){
 			player.velocity.x -= speed * dt;
 		}
+		
 
 		// --- update player position -------------
 		player.position.x += player.velocity.x * dt;
@@ -149,6 +188,9 @@ int main()
 		if(player_collided(chunks, player.position, playersize)){	
 			player.position.y -= player.velocity.y * dt;
 			player.velocity.y = 0;
+		}
+		if(player_collided(chunks, player.position, playersize)){
+			player.position.y -= speed * dt;
 		}
 
 		// --- update camera (centered around player) --------------
@@ -162,7 +204,6 @@ int main()
 		// ---- block breaking -----------
 		if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
 			breakBlock();
-
 		}else{
 			targetBlock = NULL;
 			breakProgress = 0.0f;
@@ -170,36 +211,7 @@ int main()
 
 		// ---- block placing -----------
 		if(IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)){
-			if(hoveredBlock != NULL){
-				int blockChunkIdx = chunk_index(chunk_coord(hitPosition.x));
-
-				//find direction 
-				DirectionXY_i direction = blockPlayerDirection(chunks, player.position, blockChunkIdx);
-				int targetX = direction.x;
-				int targetY = direction.y;
-
-				//place blocks
-				//find the correct chunk
-				if(targetX < 0){
-					//left chunk
-					int leftChunkIdx = getLeftChunkIndex(blockChunkIdx);
-
-					if(leftChunkIdx  >= 0 && leftChunkIdx < WORLD_SIZE){
-						targetX += CHUNK_WIDTH; 
-						blockChunkIdx = leftChunkIdx;
-					}
-				}else if(targetX >= CHUNK_WIDTH){
-					//right chunk
-					int rightChunkIdx = getRightChunkIndex(blockChunkIdx);
-
-					if(rightChunkIdx >= 0 && rightChunkIdx < WORLD_SIZE){
-						targetX -= CHUNK_WIDTH;
-						blockChunkIdx = rightChunkIdx;
-					}
-				}
-
-	            placeBlockAt(chunks, blockChunkIdx, targetX, targetY, STONE, player.position, playersize);
-			}
+			placeBlock(chunks, player.position, playersize, STONE);
 		}
 	
 		// ------------ clear and draw --------------------
@@ -216,9 +228,9 @@ int main()
 			float x = chunks[chunkIdx].position.x;
 			float y = chunks[chunkIdx].position.y;
 			if(chunk_index(chunk_coord(player.position.x)) == chunk_index(chunk_coord(x))){
-				//DrawRectangleLinesEx((Rectangle){x-CAMERA.x,y-CAMERA.y,CHUNK_WIDTH*BLOCK_WIDTH, CHUNK_HEIGHT*BLOCK_HEIGHT}, 3, BLUE);
+				DrawRectangleLinesEx((Rectangle){x-CAMERA.x,y-CAMERA.y,CHUNK_WIDTH*BLOCK_WIDTH, CHUNK_HEIGHT*BLOCK_HEIGHT}, 3, BLUE);
 			}else{
-				//DrawRectangleLinesEx((Rectangle){x-CAMERA.x,y-CAMERA.y,CHUNK_WIDTH*BLOCK_WIDTH, CHUNK_HEIGHT*BLOCK_HEIGHT}, 3, WHITE);
+				DrawRectangleLinesEx((Rectangle){x-CAMERA.x,y-CAMERA.y,CHUNK_WIDTH*BLOCK_WIDTH, CHUNK_HEIGHT*BLOCK_HEIGHT}, 3, WHITE);
 			}
 		}
 
@@ -232,27 +244,52 @@ int main()
 	UnloadTexture(grassBlock);
 	UnloadTexture(stoneBlock);
 	UnloadTexture(steve);
+	UnloadTexture(bedRock);
+	UnloadTexture(waterBlock);
+	UnloadTexture(lavaBlock);
+	UnloadTexture(ironOre);
+	UnloadTexture(goldOre);
+	UnloadTexture(emeraldOre);
+	UnloadTexture(diamondOre);
 	CloseWindow();
 	return 0;
 }
 /// ------------------- function definition --------------------
+void InitializeWorldSeed()
+{
+    worldSeedOffset = (float)GetRandomValue(-100000, 100000); 
+}
 
 void draw_chunk(Chunk* chunk)
 {
 	for(int y = 0; y < CHUNK_HEIGHT; y++){
 		for(int x = 0; x < CHUNK_WIDTH; x++){
 			int idx = x + (y * CHUNK_WIDTH);
-			if(chunk->blocks[idx].isBreak) continue;
+			bool isBreak = chunk->blocks[idx].isBreak;
+			BlockType type = chunk->blocks[idx].type;
+
+			if(type != AIR && type != WATER){
+				if(isBreak) continue;
+			}
 
 			int blockX = chunk->position.x + (x * BLOCK_WIDTH);
 			int blockY = chunk->position.y + (y * BLOCK_HEIGHT);
 
-			switch (chunk->blocks[idx].type){
-				case DIRT:
+			switch (type){
+				case GRASS:
 					DrawTexture(grassBlock, blockX-CAMERA.x , blockY-CAMERA.y, WHITE);
 					break;
 				case STONE:
 					DrawTexture(stoneBlock, blockX-CAMERA.x , blockY-CAMERA.y, WHITE);
+					break;
+				case AIR:
+				//	DrawTexture(waterBlock, blockX-CAMERA.x , blockY-CAMERA.y, WHITE);
+					break;
+				case WATER:
+					DrawTexture(waterBlock, blockX-CAMERA.x , blockY-CAMERA.y, WHITE);
+					break;
+				case BEDROCK:
+					DrawTexture(bedRock, blockX-CAMERA.x , blockY-CAMERA.y, WHITE);
 					break;
 				default:
 					break;
@@ -277,33 +314,59 @@ void draw_chunk(Chunk* chunk)
 	}
 }
 
-void init_world(Chunk chunks[], int size)
+void init_chunk(Chunk* chunk)
 {
-	Vector2 pos = {0,0};
-	for(int i = 0; i < size; i++){
-		Vector2 chunk_position = (Vector2){pos.x + (i*(CHUNK_WIDTH * BLOCK_WIDTH)), pos.y};
-		chunk_position.x -= (size/2)*(CHUNK_WIDTH*BLOCK_WIDTH);
-
-		int chunkIdx = chunk_index(chunk_coord(chunk_position.x));
-
-		chunks[chunkIdx].position = chunk_position;
-
+	for(int x = 0; x < CHUNK_WIDTH; x++){
+		float worldX = chunk->position.x + (x * BLOCK_WIDTH);
 		for(int y = 0; y < CHUNK_HEIGHT; y++){
-			for(int x = 0; x < CHUNK_WIDTH; x++){
-				int idx = x + (y * CHUNK_WIDTH);
+			int idx = x + (y * CHUNK_WIDTH);
 
-				chunks[chunkIdx].blocks[idx] = (Block){
-					.type = y < (CHUNK_HEIGHT/2)/2 ? DIRT : STONE,
-					.isBreak = false,
-					.isHover = false
-				};
+			float worldY = chunk->position.y + (y * BLOCK_HEIGHT);
+			BlockType type = noise(worldX, worldY);
 
-				chunks[chunkIdx].blocks[idx].type = GetRandomValue(0,2);
-				if(chunks[chunkIdx].blocks[idx].type == AIR){
-					chunks[chunkIdx].blocks[idx].isBreak = true;
+			chunk->blocks[idx] = (Block){0};
+			chunk->blocks[idx].isBreak = false;
+			chunk->blocks[idx].isHover = false;
+
+			if(y == CHUNK_HEIGHT-1){
+				chunk->blocks[idx].type = BEDROCK;
+			}else{
+				switch(type){
+					case AIR:
+						chunk->blocks[idx].type = AIR;
+						chunk->blocks[idx].isBreak = true;
+						break;
+					case WATER:
+						chunk->blocks[idx].type = WATER;
+						chunk->blocks[idx].isBreak = true;
+						break;
+					case STONE:
+						chunk->blocks[idx].type = STONE;
+						break;
+					case GRASS:
+						chunk->blocks[idx].type = GRASS;
+						break;
+					default:
+						printf("[TYPE ERROR]: %f (%d,%d)\n", chunk->position.x, x,y);	
+						break;
 				}
 			}
 		}
+	}
+}
+
+void init_world(Chunk chunks[], int size)
+{
+	InitializeWorldSeed();
+	Vector2 pos = {0,0};
+	for(int i = 0; i < size; i++){
+		Vector2 chunk_position = (Vector2){pos.x + (i*(CHUNK_WIDTH * BLOCK_WIDTH)), pos.y - (CHUNK_HEIGHT/2) * BLOCK_HEIGHT};
+		chunk_position.x -= (size/2)*(CHUNK_WIDTH*BLOCK_WIDTH);
+
+		int chunkIdx = chunk_index(chunk_coord(chunk_position.x));
+		chunks[chunkIdx].position = chunk_position;
+
+		init_chunk(&chunks[chunkIdx]);
 	}
 }
 
@@ -455,13 +518,16 @@ DirectionXY_i blockPlayerDirection(Chunk chunks[], Vector2 playerPosition, int b
 	int blockX = (int)(hitPosition.x - chunks[blockChunkIdx].position.x) / BLOCK_WIDTH;
 	int blockY = (int)(hitPosition.y - chunks[blockChunkIdx].position.y) / BLOCK_HEIGHT;
 
+	float blockWorldX = hitPosition.x;
+	float playerWorldX = playerPosition.x;	
+
 	int playerX = (int)(playerPosition.x - chunks[blockChunkIdx].position.x) / BLOCK_WIDTH;
 	int playerY = (int)(playerPosition.y - chunks[blockChunkIdx].position.y) / BLOCK_WIDTH;
 
 	int targetX = -1;
 	int targetY = -1;
 
-	if(blockX == playerX){
+	if(blockWorldX == playerWorldX){
 		//up or down
 		if(blockY < playerY){
 			targetX = blockX;
@@ -470,7 +536,7 @@ DirectionXY_i blockPlayerDirection(Chunk chunks[], Vector2 playerPosition, int b
 			targetX = blockX;
 			targetY = blockY -1;
 		}
-	}else if(blockX < playerX){
+	}else if(blockWorldX < playerWorldX){
 		//left 
 		if(blockY <= playerY){
 			//up or same height
@@ -541,7 +607,7 @@ DirectionXY_i blockPlayerDirection(Chunk chunks[], Vector2 playerPosition, int b
 				}	
 			}
 		}
-	}else if(blockX > playerX){
+	}else if(blockWorldX > playerWorldX){
 		//right
 		if(blockY <= playerY){
 			//up or same level
@@ -551,7 +617,7 @@ DirectionXY_i blockPlayerDirection(Chunk chunks[], Vector2 playerPosition, int b
 				targetY = blockY + 1;//if not below, below
 				if(!isBlockBreak(chunks, targetX, targetY, blockChunkIdx)){
 					//left
-					targetX = blockX - 1;
+					targetX = blockX - 1;  
 					targetY = blockY;
 				}
 			}else{
@@ -570,7 +636,7 @@ DirectionXY_i blockPlayerDirection(Chunk chunks[], Vector2 playerPosition, int b
 						targetY = blockY + 1;//if not down, down
 						if(!isBlockBreak(chunks, targetX, targetY, blockChunkIdx)){
 							//left
-							targetX = blockX - 1;
+							targetX = blockX - 1; 
 							targetY = blockY;
 						}
 					}else{
@@ -587,12 +653,12 @@ DirectionXY_i blockPlayerDirection(Chunk chunks[], Vector2 playerPosition, int b
 				targetY = blockY - 1;//if not up, up
 				if(!isBlockBreak(chunks, targetX, targetY, blockChunkIdx)){
 					//left
-					targetX = blockX - 1;
+					targetX = blockX - 1; //Issue here
 					targetY = blockY;	
 				}
 			}else{
 				if(blockX > playerX+1){
-					//more than 2 block right	
+					// more than 2 block right	
 					//left
 					targetX = blockX - 1;//if not left, left
 					targetY = blockY;
@@ -603,10 +669,10 @@ DirectionXY_i blockPlayerDirection(Chunk chunks[], Vector2 playerPosition, int b
 					}
 				}else{
 					targetX = blockX;
-					targetY = blockY - 1;//if not up, up
+					targetY = blockY - 1; //if not up, up
 					if(!isBlockBreak(chunks, targetX, targetY, blockChunkIdx)){
-						//left
-						targetX = blockX - 1;
+						// left
+						targetX = blockX - 1; 
 						targetY = blockY;
 					}
 				}
@@ -619,7 +685,7 @@ DirectionXY_i blockPlayerDirection(Chunk chunks[], Vector2 playerPosition, int b
 
 void breakBlock()
 {
-	if(hoveredBlock != NULL){
+	if(hoveredBlock != NULL && hoveredBlock->type != BEDROCK){
 
 		if(targetBlock != hoveredBlock){
 			targetBlock = hoveredBlock;
@@ -636,6 +702,40 @@ void breakBlock()
 	}else{
 		targetBlock = NULL;
 		breakProgress = 0.0f;
+	}
+}
+
+void placeBlock(Chunk chunks[], Vector2 playerPos, Vector2 playerSize, BlockType type)
+{
+	if(hoveredBlock != NULL){
+		int blockChunkIdx = chunk_index(chunk_coord(hitPosition.x));
+
+		//find direction 
+		DirectionXY_i direction = blockPlayerDirection(chunks, playerPos, blockChunkIdx);
+		int targetX = direction.x;
+		int targetY = direction.y;
+
+		//place blocks
+		//find the correct chunk
+		if(targetX < 0){
+			//left chunk
+			int leftChunkIdx = getLeftChunkIndex(blockChunkIdx);
+
+			if(leftChunkIdx  >= 0 && leftChunkIdx < WORLD_SIZE){
+				targetX += CHUNK_WIDTH; 
+				blockChunkIdx = leftChunkIdx;
+			}
+		}else if(targetX >= CHUNK_WIDTH){
+			//right chunk
+			int rightChunkIdx = getRightChunkIndex(blockChunkIdx);
+
+			if(rightChunkIdx >= 0 && rightChunkIdx < WORLD_SIZE){
+				targetX -= CHUNK_WIDTH;
+				blockChunkIdx = rightChunkIdx;
+			}
+		}
+
+        placeBlockAt(chunks, blockChunkIdx, targetX, targetY, type , playerPos, playerSize);
 	}
 }
 
@@ -697,6 +797,63 @@ void findHoveredBlock(Chunk chunks[], Vector2 playerPos, Vector2 playerSize)
 		}
 		if(rayHitSomething) break;
 	}
+}
+
+BlockType noise(float wX, float wY)
+{
+    float SEA_LEVEL_Y = 0.0f;
+    float BASE_HEIGHT = (float)CHUNK_HEIGHT / 2.0f;
+
+    // 1. DOMAIN WARPING (Breaks the predictable sine symmetry)
+    // Instead of raw sin(x), we warp the coordinate space itself using another sine 
+    // offset. This bends the hills, stretches valleys, and removes the repeating look.
+    float warp = sin((wX + worldSeedOffset) * 0.01f) * 15.0f;
+    float warpedX = wX + worldSeedOffset+ warp;
+
+    // 2. ORGANIC HEIGHT SYNTHESIS 
+    // Using varied non-integer multipliers so the waves never line up uniformly.
+    float hills = sin(warpedX * 0.0038f) * 142.0f;
+    float details = sin(warpedX * 0.017f) * 112.0f;
+    float roughness = sin(warpedX * 0.053f) * 63.5f;
+
+    float surfaceHeightY = SEA_LEVEL_Y - (BASE_HEIGHT + hills + details + roughness);
+
+    // 3. SKY / AIR CHECK
+    if (wY < surfaceHeightY) {
+        return (BlockType)AIR;
+    }
+
+    // 4. UNDERGROUND CAVE POCKETS (Winding structural tubes)
+    // Using an intersecting sine/cosine grid offset by world position
+    if (wY > surfaceHeightY + 8.0f) {
+        float caveVal1 = sin((wX + worldSeedOffset) * 0.035f) * sin(wY * 0.035f);
+        float caveVal2 = cos((wY - wX) * 0.025f); // Diagonal tearing factor
+
+        // Carve out tunnels where waves intersect cleanly
+        if ((caveVal1 > 0.55f && caveVal2 > 0.2f) || (caveVal1 < -0.55f && caveVal2 < -0.2f)) {
+            return (BlockType)AIR;
+        }
+    }
+
+    // 5. FLUID / UNDERGROUND POOLS
+    // Controlled basin check deep down
+    float fluidPocket = cos((wX + worldSeedOffset) * 0.009f);
+    if (wY > (BASE_HEIGHT + 45.0f) && wY < (BASE_HEIGHT + 65.0f) && fluidPocket > 0.75f) {
+        return (BlockType)WATER;
+    }
+
+    // 6. BLOCK VARIETY LAYERING (Reference Guide for your blocks)
+    float depth = wY - surfaceHeightY;
+
+    if (depth < 1.0f) {
+        return (BlockType)GRASS; // Top layer (You can change to Grass later if wY == surfaceHeightY)
+    } 
+    else if (depth < 5.0f) {
+        return (BlockType)GRASS; // Sub-surface soil layer
+    }
+
+    // Everything else deep underneath is solid stone
+    return (BlockType)STONE;
 }
 
 void add_chunk(Chunk chunks[], Vector2 pos)
